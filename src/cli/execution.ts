ff87cb7ffff6a4853543147638086b87b3c52272
Victor Perin <me@victorperin.ninja>
2024-10-25 17:50:41 -0300

    feat(cli): connect to analyzer

--- src/cli/execution.ts
+++ src/cli/execution.ts
@@ -1,17 +1,30 @@
 import yargs from 'yargs'
 import { flags } from './flags'
+import { loadHierarchy } from '../analyser/hierarchyLoader'
+import { analyzePhrase } from '../analyser';
 
 
-export const execution = (args: string[]) => {
-    const cliInstance = yargs(args)
-        .strict()        
-        .demandCommand( // Set yargs to receive just one phrase, nothing more
-            1,
-            1,
-            'A phrase must be passed to be analyzed'
-        )
-        .options(flags)
-        .parse()
+export const parseArguments = async (args: string[]) =>
+  yargs(args)
+    .strict()        
+    .demandCommand( // Set yargs to receive just one phrase, nothing more
+      1,
+      1,
+      'A phrase must be passed to be analyzed'
+    )
+    .options(flags)
+    .parse()
 
-    console.log(cliInstance);
-}
+
+export const execution = async (args: string[]) => {
+  const { depth, verbose, _: [phrase] } = await parseArguments(args);
+
+  if (!phrase) {
+    console.error('Por favor, forneça uma frase válida para análise.');
+    process.exit(1);
+  }
+
+  const hierarchy = await loadHierarchy('./dicts/hierarquia.json');
+  const result = analyzePhrase(phrase.toString(), Number(depth), hierarchy);
+  console.log(result)
+  };
