25a5762f668bd7bb343ebd1fa800060a89f28096
Victor Perin <me@victorperin.ninja>
2024-10-25 13:32:40 -0300

    test(analyzer/findDepth): add todo tests

---
+++ src/analyser/findDepth.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from "bun:test";
+
+describe('findDepth', () => {
+  test.todo('Deve encontrar Mamíferos para Gorilas em nível 2')
+  test.todo('Deve encontrar Bovídeos para Bois em nível 3')
+  test.todo('Deve encontrar Leões para Leões em nível 5')
+  test.todo('Não deve encontrar nada Bois em nível 100')
+  test.todo('Não deve encontrar nada Aves em nível 5')
+  test.todo('Não deve encontrar nada Aves em nível 5')
+  test.todo('Não deve encontrar nada Desconhecido em nível 1')
+})
